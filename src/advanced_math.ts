f255796edd01cd2648215cec9373cbc68ed17337
Sanders Lauture <golf1052@msn.com>
2016-08-10 13:40:34 -0400

    Add more numbers

--- src/advanced_math.ts
+++ src/advanced_math.ts
@@ -2,7 +2,7 @@ const add1 = require('add-1-to-a-number');
 
 export function add(num: number | string): number | string {
     try {
-        let val = add1(num);
+        let val = add1(add1(num));
         return val;
     }
     catch (ex) {
